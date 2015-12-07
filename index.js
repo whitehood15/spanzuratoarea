8692510cccc6bf54b65a885cd157d97968fc9401
whitehood15 <whitehood15@gmail.com>
2015-12-07 20:54:42 +0200

    added registration pages

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 
-
-function existsInDB(usr,pwd){
+function existsInDB(usr, pwd){//needs editing when mysql functionality installed
 	if(usr=="me"&&pwd=="hi")return 1;
 	else return 0;
-
 }
+function insertUser()
 
 var app = express();
 var handlebars = require('express3-handlebars')
@@ -15,7 +14,9 @@ app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port',process.env.PORT || 3000 );
 
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.get('/', function(req, res){
 	var id = req.param('usr');
 	var password= req.param('pwd');
@@ -32,6 +33,21 @@ app.use(function(req,res,next){
 	res.type('text/plain');
 	res.status(404);
 	res.send('404 - Not found');
+});
+app.get('/register',function (req, res){
+	res.render('register');
+});
+app.post('/register/new',function (req,res){
+	var nickname=req.body.nick;
+	var email=req.body.email;
+	var password = req.body.password;
+	var age = req.body.age;
+	if(!(nickname&&email&&password&&age))
+		res.render('regfail');
+	if(insertUser(nickname,email,password,age));
+		res.render('regsuccess');
+	else
+		res.render('regfail');
 });
 app.listen(app.get('port'),function(){
 	console.log( 'Express started at http://localhost:'+

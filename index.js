3fc78f0320b494c21f91890f3411c33619e0e422
whitehood15 <whitehood15@gmail.com>
2015-12-08 22:59:41 +0200

    added some func

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-
+var cookieParser = require('cookie-parser');
+var expressSanitizer = require('express-sanitizer');
 function existsInDB(usr, pwd){//needs editing when mysql functionality installed
 	if(usr=="me"&&pwd=="hi")return 1;
 	else return 0;
@@ -14,15 +15,22 @@ app.use(express.static(__dirname+'/public'));
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port',process.env.PORT || 3000 );
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressSanitizer[options]);
+app.use(cookieParser());
 app.get('/', function(req, res){
-	var id = req.param('usr');
-	var password= req.param('pwd');
-	if(existsInDB(id,password))
+	
+		var id =  req.sanitize(req.param('usr'));
+		var password=  req.sanitize(req.param('pwd'));
+	
+	if(existsInDB(id,password)){
+		res.cookie('spanzusr',id);
+		res.cookie('spanzpwd',password);
 		res.render('gamepage',{pagename: 'Game',style:"game.css"});
+	}
 	else
 		res.render('login',{pagename: 'Login',style:"login.css"});
 }); 
@@ -35,10 +43,10 @@ app.get('/register',function (req, res){
 	res.render('register',{pagename:'Inregistrare',style:'register.css'});
 });
 app.post('/register/new',function (req,res){
-	var nickname=req.body.nick;
-	var email=req.body.email;
-	var password = req.body.password;
-	var age = req.body.age;
+	var nickname= req.sanitize(req.body.nick);
+	var email= req.sanitize(req.body.email);
+	var password =  req.sanitize(req.body.password);
+	var age =  req.sanitize(req.body.age);
 	if(!(nickname&&email&&password&&age))
 		res.render('regfail',{pagename:'Eroare',style:'regfail.css'}); 
 	else{

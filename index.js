614dd90229b6c10bbbd3f709826accaf693a2604
whitehood15 <whitehood15@gmail.com>
2016-01-15 21:29:40 +0100

    added login mechanism

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
-function existsInDB(usr, pwd){//needs editing when mysql functionality installed
-	if(usr=="me@gmail.com"&&pwd=="hi")return 1;
-	else return 0;
-}
-function insertUser(nickname,email,password,age){
-	return 1;
-}
 var app = express();
 var handlebars = require('express3-handlebars')
 			.create({ defaultLayout:'main'});
-			
 app.use(express.static(__dirname+'/public'));
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
@@ -19,21 +11,44 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser());
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.use(cookieParser());
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/test');
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+	nickname:String,
+	email:String,
+	password:String,
+	age:Number,
+	rank:Number,
+	games:Number,
+	points:Number	
+});
+var User = mongoose.model("User",UserSchema);
 app.get('/', function(req, res){
-	
-		var id =  req.param('usr');
-		var password=  req.param('pwd');
-	
-	if(existsInDB(id,password)){
-		res.cookie('spanzusr',id);
-		res.cookie('spanzpwd',password);
-		res.render('gamepage',{pagename: 'Game',style:"game.css"});
+	if(req.cookies.Spanz){
+		User.findById(req.cookies.Spanz,function(err, user){
+			if(user)		
+			res.render('gamepage',{pagename: 'Game',style:"game.css"});
+		});	
+	}else{
+		if(req.query.usr&&req.query.pwd){
+			User.findOne({"email":req.query.usr,"password":req.query.pwd},function(err,user) {
+				if(user){
+					console.log(user);
+					res.cookie("Spanz",user._id);
+					res.render('gamepage',{pagename: 'Game',style:"game.css"});
+				}else
+					res.render('login',{pagename: 'Login',style:"login.css"});
+			});
+
+			}else{
+		
+			res.render('login',{pagename: 'Login',style:"login.css"});
+		}
 	}
-	else
-		res.render('login',{pagename: 'Login',style:"login.css"});
 }); 
+
 app.get('/game', function(req, res){
 	if(req.xhr==true){
 	res.type('application/json');
@@ -46,18 +61,17 @@ app.get('/register',function (req, res){
 	res.render('register',{pagename:'Inregistrare',style:'register.css'});
 });
 app.post('/register/new',function (req,res){
-	var nickname= req.body.nick;
-	var email= req.body.email;
-	var password =  req.body.password;
-	var age =  req.body.age;
-	if(!(nickname&&email&&password&&age))
-		res.render('regfail',{pagename:'Eroare',style:'regfail.css'}); 
-	else{
-		if(insertUser(nickname,email,password,age))
-			res.render('regsuccess',{pagename:'Succes',style:'regsuccess.css'});
-		else
-			res.render('regfail',{pagename:'Eroare',style:'regfail.css'});
-	}
+	var nick= req.body.nick;
+	var emailin= req.body.email;
+	var passwordin =  req.body.password;
+	var agein =  req.body.age;
+	var newUser = new User({ nickname: nick,email:emailin,password:passwordin,age:agein });
+	console.log(newUser);
+		newUser.save(function (err) {
+		  if (err) res.send("error");
+		  res.send("OK");
+		})
+
 });
 app.use(function(req,res,next){	
 	res.status(404);
